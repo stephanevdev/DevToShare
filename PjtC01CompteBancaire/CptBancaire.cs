eb1d09e8ac06888538bb64b5b1e61df9e253b1c1
stephv <tstmz8@free.fr>
2021-06-30 16:28:43 +0200

    Compte Bancaire Etape1 première mise à dispo

---
+++ PjtC01CompteBancaire/CptBancaire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PjtC01CompteBancaire
+{
+    class CptBancaire
+    {
+        public long numcpt;
+        public string nomcli;
+        public double solde;
+        public double decouvertAutorise;
+
+        public void Crediter(double montant)
+        {
+            solde = solde + montant;
+
+        }
+        public void Debiter(double montant)
+        {
+            solde = solde + montant;
+
+        }
+    }
+}
---
+++ PjtC01CompteBancaire/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PjtC01CompteBancaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CptBancaire cptToto; //déclaration de l'objet 
+            CptBancaire cpt;
+            cptToto = new CptBancaire(); // Instanciation de l'objet cptToto
+            cpt = new CptBancaire();
+
+            //Instantciation des attibuts pour Etape 1?
+
+            cptToto.numcpt = 12345;
+            cptToto.nomcli = "Toto";
+            cptToto.solde = 1000;
+            cptToto.decouvertAutorise = -500;
+
+
+            //Appel des méthodes créditer et débiter;
+
+            cptToto.Crediter(2000);
+            cptToto.Debiter(0);
+
+
+            //Affichage
+            //Affichage
+
+            string msg = "numéro : " + cptToto.numcpt.ToString() + " nom : " + cptToto.nomcli + " solde " + cptToto.solde.ToString() + " decouvertAutorisé : " + cptToto.decouvertAutorise.ToString() + ".";
+            Console.WriteLine(msg);
+            Console.Read();
+        }
+    }
+}
