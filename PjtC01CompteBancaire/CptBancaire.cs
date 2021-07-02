f86fb34d27783b95ebcb6e1c9ffe4064a3d122e2
stephv <tstmz8@free.fr>
2021-07-02 08:15:30 +0200

    C# Compte Bancaire
    Version1 de l'exercice compte bancaire complet en C#

--- PjtC01CompteBancaire/CptBancaire.cs
+++ PjtC01CompteBancaire/CptBancaire.cs
@@ -1,27 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PjtC01CompteBancaire
 {
     class CptBancaire
     {
-        public long numcpt;
-        public string nomcli;
-        public double solde;
-        public double decouvertAutorise;
+        private long numcpt;
+        private string nomcli;
+        private double solde;
+        private double decouvertAutorise;
 
+        //Constructeur
+        public CptBancaire(long _numcpt, string _nomcli, double _solde, double _decouvertAutorise)
+        {
+            this.numcpt = _numcpt;
+            this.nomcli = _nomcli;
+            this.solde = _solde;
+            this.decouvertAutorise = _decouvertAutorise;
+
+        }
         public void Crediter(double montant)
         {
-            solde = solde + montant;
+            solde += montant;
 
         }
-        public void Debiter(double montant)
+        bool debite = false;
+        string msg;
+        public bool Debiter(double montant)
         {
-            solde = solde + montant;
+           
+            if ((montant + decouvertAutorise) > solde)
+            {
+                
+                msg = "Debit refusé";
+                return false;
+            }
 
+
+            else
+            {
+                solde -= montant;
+                msg = ("Débit Réussi!");
+                return true;
+            }
+           
         }
+        public bool SoldeCpt( double sold1, double sold2)
+        {
+            if (sold1 > sold2)
+            {
+                msg = "Supérieur";
+                return true;
+                
+            }
+            else
+            {
+                msg = "Inférieur";
+                return false;
+            }
+        }
+        public void Afficher()
+        {
+            msg = msg + " // numéro : " + numcpt.ToString() + " nom : " + nomcli + " solde " + solde.ToString() + " decouvertAutorisé : " + decouvertAutorise.ToString() + ".";
+            Console.WriteLine(msg);
+        }
+
+
     }
 }
+        
--- PjtC01CompteBancaire/Program.cs
+++ PjtC01CompteBancaire/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PjtC01CompteBancaire
 {
@@ -10,31 +6,44 @@ namespace PjtC01CompteBancaire
     {
         static void Main(string[] args)
         {
-            CptBancaire cptToto; //déclaration de l'objet 
-            CptBancaire cpt;
-            cptToto = new CptBancaire(); // Instanciation de l'objet cptToto
-            cpt = new CptBancaire();
+            CptBancaire cptToto = new CptBancaire(12345, "Toto", 1000, -500);
+            CptBancaire cptLaurent = new CptBancaire(545454, "Laurent", 2000, -500);
 
-            //Instantciation des attibuts pour Etape 1?
+            //Appel des méthodes créditer et débiter
 
-            cptToto.numcpt = 12345;
-            cptToto.nomcli = "Toto";
-            cptToto.solde = 1000;
-            cptToto.decouvertAutorise = -500;
-
-
-            //Appel des méthodes créditer et débiter;
-
-            cptToto.Crediter(2000);
+            //cptToto.Crediter(2000);
             cptToto.Debiter(0);
 
-
-            //Affichage
             //Affichage
 
             string msg = "numéro : " + cptToto.numcpt.ToString() + " nom : " + cptToto.nomcli + " solde " + cptToto.solde.ToString() + " decouvertAutorisé : " + cptToto.decouvertAutorise.ToString() + ".";
             Console.WriteLine(msg);
+            msg = "numéro : " + cptLaurent.numcpt.ToString() + " nom : " + cptLaurent.nomcli + " solde " + cptLaurent.solde.ToString() + " decouvertAutorisé : " + cptLaurent.decouvertAutorise.ToString() + ".";
+            Console.WriteLine(msg);
+
+          
+            cptLaurent.Crediter(100);
+
+            cptLaurent.Debiter(100000);
+
+            cptLaurent.Afficher();
+            CptBancaire cptTiti = new CptBancaire(45657, "Titi", 2000, -1000);
+
+            bool debite =cptToto.Debiter(3300);
+            if (debite)
+            {
+                cptTiti.Crediter(3300);
+            }
+
+            bool sup = cptToto.SoldeCpt(cptToto.solde, cptTiti.solde);
+               
+
+            
+            cptToto.Afficher();
+            cptTiti.Afficher();
+
             Console.Read();
+
         }
     }
 }
